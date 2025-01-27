2f88fbbd2cd9955450e6670892574a344d362181
billn <billnyetherussianspy679@gmail.com>
2025-01-27 15:05:53 +0300

    saving changes

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,32 @@
 // Your tests here
+import {isPalindrome, pointsForWord} from "../utils.js"
+import {pointsForWord} from "../utils.js"
+describe("isPalindrome", () => {
+        it("checks whether a word is a palindrome or not", () => {
+           const word= 'racecar'
+            const result=isPalindrome(word);
+           expect(result).toBe(true);
+        });
+    const word= 'car'
+    const result=isPalindrome(word);
+    expect(result).toBe(false);
+});
+it("return true for words that are a combination of uppercase and lowercase letters", () => {
+    const word = "RaCecar";
+
+    const result = isPalindrome(word);
+
+    expect(result).toBe(true);
+});
+it("throws an error if the input has any non-alphabetic characters", () => {
+    const word = "race car";
+
+    expect(() => isPalindrome(word)).toThrow();
+});
+it("throws an error if the input is an empty string", () => {
+    const word = "";
+
+    expect(() => isPalindrome(word)).toThrow();
+});
+
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,26 @@
 // Your code here
+export function isPalindrome(word){
+    if (!/^[A-Za-z]+$/.test(word)) {
+        throw new Error("Invalid input");
+    }
+
+    const normalisedWord=word.toLowerCase();
+    const reversedWord=normalisedWord.split('').reverse().join('');
+
+    if(normalisedWord!==reversedWord || normalisedWord===''){
+        return false;
+    }
+    else{
+        return true;
+    }
+
+}
+console.log(isPalindrome('madam'));
+
+export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+        points += /[aeiou]/i.test(char) ? 1 : 2;
+    }
+    return points;
+}
